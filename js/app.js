b8c053922cd0a0b671bf9a63c9c58dd7c211cf76
Dzaky Rafi Syahputra <dzakylmg87@gmail.com>
2024-01-15 13:31:29 +0700

    test polaroid

--- js/app.js
+++ js/app.js
@@ -996,3 +996,20 @@ const comment = (() => {
         kirim,
     };
 })();
+
+$('figure').addClass("next").first().removeClass("next").addClass("current");
+
+setInterval(function() { 
+  var nxt = $('figure.current')
+    .removeClass("current")
+    .addClass("prev")
+    .next();
+  if(nxt.length == 0) {
+    
+    nxt = $('figure:first');
+    $('figure:gt(0)').addClass('next').removeClass('prev');
+  }
+  
+    nxt.addClass("current")
+    .removeClass("next");
+},  2000);
